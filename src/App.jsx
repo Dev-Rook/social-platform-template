c41ef76b8f85c9fd8f86dc6554c55e19a6038c6d
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-11-04 23:28:21 -0500

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import Styles from "./App.module.css"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Styles from "./App.module.css";
 
+
+// Component Import Start
+import Navbar from "./Components/Main/Navbar"
+import CreatePost from "./Components/Main/CreatePost"
+import Mode from "./Components/Main/Mode"
+import Footer from "./Components/Main/Footer"
+// Component Import End
+
+// Pages Import Start
+import Landing from "./Pages/Landing"
+import Login from "./Pages/Login"
+import Error from "./Pages/Error"
+// Pages Import End
 
 const App = () => {
   return (
     <div className={Styles.App}>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Landing />} />
+          <Route path={"Login"} element={<Login />} />
+          <Route path={"*"} element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
